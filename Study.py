2797a72428f05b3bb74688d3e5807da014f7771b
NikitaOnly <Nikita.Dechko@gmail.com>
2022-12-10 16:14:50 +1200

    For study

--- Study.py
+++ Study.py
@@ -1,27 +1,11 @@
-# Вычислить количество отрицательных элементов под главной диагональю матрицы.
-main_matrix = []
-rows = int(input('Введите количество строк: '))
-columns = int(input('Введите количество стоблцов '))
-for i in range(rows):
-    matrix = []
-    for j in range(columns):
-        elements = int(input())
-        matrix.append(elements)
-    main_matrix.append(matrix)
-for i in main_matrix:
-    print(i)
-print()
-low_diagonal = []
-for i in range(rows):
-    for j in range(columns):
-        if i > j:
-            low_diagonal.append(main_matrix[i][j])
-print(low_diagonal, end=' ')
-amount_low_diagonal = []
-negative = 0
-for i in range(len(low_diagonal)):
-    if low_diagonal[i] < 0:
-        negative += 1
-        amount_low_diagonal.append(low_diagonal[i])
-print('Количество отрицательных элементов', negative)
+# Дана строка. Сохранить в ней только первые вхождения символов, удалив все остальные.
+# Результат вывести в виде кортежа.
+string = input('Введите строку')
+turtle = ()
+for i in range(len(string)):
+    if string[i] not in turtle:
+       turtle += (string[i],)
+print(turtle)
+turtle_1 = ('hello world')
+print(turtle_1)
 
